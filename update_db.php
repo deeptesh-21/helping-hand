7c61d584fa09d4783a20a1b427953e6ede4150ce
deeptesh-21 <deepteshchari4004@gmail.com>
2023-04-20 19:41:09 +0530

    made corrections

--- update_db.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $hostname = "localhost";
-    $username = "root";
-    $password = "";
-    $database = "techothon";
-
-    $conn = mysqli_connect($hostname, $username, $password, $database) or die("Unable to connect to database");
-?>
