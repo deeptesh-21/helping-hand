a3d2c1bc0d57ce60cb7775d995faed205b07b298
deeptesh-21 <deepteshchari4004@gmail.com>
2023-04-21 09:34:25 +0530

    added

---
+++ generate_reports.php
@@ -0,0 +1,16 @@
+<?php
+    $hostname = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "technothon";
+
+    $conn = mysqli_connect($hostname, $username, $password, $database) or die("Unable to connect to database");
+
+    $query = "SELECT * FROM patientinfo";
+
+    $result = mysqli_query($conn, $query) or die("Unable to select data");
+
+    while($row = mysqli_fetch_array($result)) {
+        echo $row['data'];
+    }
+ ?>
